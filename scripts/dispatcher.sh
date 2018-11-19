2b1288b5ba9172efda707b8f395d205472342cda
Windfarer <windfarer@gmail.com>
2018-11-19 15:44:39 +0800

    rename

--- scripts/dispatcher.sh
+++ scripts/dispatcher.sh
@@ -1,7 +1,7 @@
 $SPARK_HOME/bin/spark-class \
     org.apache.spark.deploy.mesos.MesosClusterDispatcher \
     --master ${SPARK_MASTER}  \
-    --host ${LIBPROCESS_IP} \
+    --host ${PUBLIC_IP} \
     --port 7070 \
     --webui-port 4040 \
     --properties-file ${SPARK_HOME}/conf/spark-defaults.conf
--- scripts/run.sh
+++ scripts/run.sh
@@ -3,14 +3,13 @@
 SPARK_MASTER=${SPARK_MASTER:-local}
 MESOS_EXECUTOR_CORE=${MESOS_EXECUTOR_CORE:-0.1}
 SPARK_IMAGE=${SPARK_IMAGE}
-# CURRENT_IP=$(hostname -i)
 
 sed -i 's;SPARK_MASTER;'$SPARK_MASTER';g' /spark/conf/spark-defaults.conf
 sed -i 's;MESOS_EXECUTOR_CORE;'$MESOS_EXECUTOR_CORE';g' /spark/conf/spark-defaults.conf
 sed -i 's;SPARK_IMAGE;'$SPARK_IMAGE';g' /spark/conf/spark-defaults.conf
-sed -i 's;CURRENT_IP;'$CURRENT_IP';g' /spark/conf/spark-defaults.conf
+sed -i 's;PUBLIC_IP;'$PUBLIC_IP';g' /spark/conf/spark-defaults.conf
 
-export SPARK_LOCAL_IP=${SPARK_LOCAL_IP:-${CURRENT_IP:-"127.0.0.1"}}
+export SPARK_LOCAL_IP=${SPARK_LOCAL_IP:-${PUBLIC_IP:-"127.0.0.1"}}
 export SPARK_PUBLIC_DNS=${SPARK_PUBLIC_DNS:-${SPARK_PUBLIC_DNS:-"127.0.0.1"}}
 
 if [ $ADDITIONAL_VOLUMES ];
