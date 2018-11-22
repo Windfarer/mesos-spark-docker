2c0e3a23a172b7210ec324ed38e1ff16043f3996
Windfarer <windfarer@gmail.com>
2018-11-22 10:52:41 +0800

    cleaning

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-
-SPARK_MASTER=${SPARK_MASTER:-local}
-MESOS_EXECUTOR_CORE=${MESOS_EXECUTOR_CORE:-0.1}
-SPARK_IMAGE=${SPARK_IMAGE}
-
-export SPARK_LOCAL_IP=${SPARK_LOCAL_IP:-${PUBLIC_IP:-"127.0.0.1"}}
-export SPARK_PUBLIC_DNS=${SPARK_PUBLIC_DNS:-${SPARK_PUBLIC_DNS:-"127.0.0.1"}}
-
 if [ $ADDITIONAL_VOLUMES ];
 then
         echo "spark.mesos.executor.docker.volumes: $ADDITIONAL_VOLUMES" >> /spark/conf/spark-defaults.conf
 fi
 
-exec $@
+exec "$@"
